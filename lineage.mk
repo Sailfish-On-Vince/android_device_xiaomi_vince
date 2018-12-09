92f781ba0f73e1967daff2fcd484b015042ef2be
haran <harangovindan@gmail.com>
2018-12-09 11:53:51 +0200

    vince: Resolve vendor mismatch issue
    * Remove those marks sets this separate to PRODUCT_BUILD_PROP_OVERIDES
      making vendor mismatch to be fixed

--- lineage.mk
+++ lineage.mk
@@ -28,4 +28,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="vince-user 8.1.0 OPM1.171019.019 8.12.13 test-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "xiaomi/vince/vince:8.1.0/OPM1.171019.019/8.12.13:user/test-keys"
+BUILD_FINGERPRINT := xiaomi/vince/vince:8.1.0/OPM1.171019.019/8.12.13:user/test-keys
