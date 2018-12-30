33ecab25d1d8b6064b30ec202d199612b348d948
Rama Bondan Prakoso <ramanarubp@gmail.com>
2018-12-30 08:32:48 +0000

    vince: Drop prebuilt audio

--- device.mk
+++ device.mk
@@ -24,12 +24,6 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 # Inherit from msm8953-common
 $(call inherit-product, device/xiaomi/msm8953-common/msm8953.mk)
 
-# Audio Hax
-PRODUCT_PACKAGES += \
-    libtinyalsa \
-    libtinycompress \
-    libtinyxml
-
 # Audio configuration
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
