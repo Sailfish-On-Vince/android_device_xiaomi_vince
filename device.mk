4b4b62473e84712af7268d71958ada9f84250194
Rama Bondan Prakoso <ramanarubp@gmail.com>
2018-12-30 08:30:17 +0000

    vince: audio: Use stock acdb table from miui

--- device.mk
+++ device.mk
@@ -33,7 +33,7 @@ PRODUCT_PACKAGES += \
 # Audio configuration
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
-    $(LOCAL_PATH)/audio/mixer_paths_vince.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_vince.xml
+    $(LOCAL_PATH)/audio/mixer_paths_mtp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_mtp.xml
 
 # Ramdisk
 PRODUCT_PACKAGES += \
